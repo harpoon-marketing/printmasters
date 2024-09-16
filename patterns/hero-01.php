f6aea83323b21ac2b32efa9e6c0cc948075844bb
Marius P. <marius.paduraru@gmail.com>
2024-09-16 20:18:03 +0100

    Add new hero patterns and update theme configuration
    Renamed hero-01.php to hero-02.php and added new hero-03.php pattern. Updated theme.json to include the new patterns. This enhances the flexibility and design options available within the Printmasters theme.

---
+++ patterns/hero-01.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Title: Hero #01
+ * Slug: printmasters/hero-01
+ * Categories: pm
+ * Viewport width: 1200
+ */
+?>
+
+<!-- wp:printmasters/flex-container {"direction":{"directionMobile":"col","directionTablet":"col","directionDesktop":"row"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|16"}}}} -->
+<div class="wp-block-printmasters-flex-container items-stretch tablet--items-stretch desktop--items-stretch content-start tablet--content-start desktop--content-start flex-col tablet--flex-col desktop--flex-row justify-start tablet--justify-start desktop--justify-start flex-no-wrap tablet--flex-no-wrap desktop--flex-no-wrap has-global-margin-bottom"
+     style="margin-top:var(--wp--preset--spacing--16)">
+    <!-- wp:printmasters/flex-item {"alignItems":{"itemsMobile":"stretch","itemsTablet":"stretch","itemsDesktop":"start"},"direction":{"directionMobile":"col","directionTablet":"col","directionDesktop":"col"},"alignSelf":{"selfMobile":"auto","selfTablet":"auto","selfDesktop":"center"},"order":{"orderMobile":"1","orderTablet":"1","orderDesktop":0},"style":{"spacing":{"blockGap":"var:preset|spacing|48","padding":{"right":"var:preset|spacing|24"}}}} -->
+    <div class="wp-block-printmasters-flex-item items-stretch tablet--items-stretch desktop--items-start content-start tablet--content-start desktop--content-start flex-col tablet--flex-col desktop--flex-col justify-start tablet--justify-start desktop--justify-start flex-no-wrap tablet--flex-no-wrap desktop--flex-no-wrap self-auto tablet--self-auto desktop--self-center grow-0 tablet--grow-0 desktop--grow-0 shrink tablet--shrink desktop--shrink order-1 tablet--order-1 desktop--order-0"
+         style="padding-right:var(--wp--preset--spacing--24);--flex-basis:100%;--flex-basis-tablet:100%;--flex-basis-desktop:50%">
+        <!-- wp:group {"layout":{"type":"constrained","justifyContent":"left"}} -->
+        <div class="wp-block-group"><!-- wp:heading {"level":1} -->
+            <h1 class="wp-block-heading">Personalization services for promotional products, in a <span
+                        class="is-primary">one-stop-shop</span> platform.</h1>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph {"style":{"typography":{"fontSize":"24px","lineHeight":"1.3"}}} -->
+            <p style="font-size:24px;line-height:1.3">We know all the ins and outs of imprinting for all techniques
+                offered.</p>
+            <!-- /wp:paragraph --></div>
+        <!-- /wp:group -->
+
+        <!-- wp:printmasters/buttons -->
+        <div class="wp-block-printmasters-buttons justify-start tablet--justify-start desktop--justify-start">
+            <!-- wp:printmasters/button {"buttonText":"Learn more","style":{"elements":{"link":{"color":{"text":"var:preset|color|neutral-100"}}}},"backgroundColor":"primary","textColor":"neutral-100"} -->
+            <a class="wp-block-printmasters-button has-neutral-100-color has-primary-background-color has-text-color has-background has-link-color is-filled has-icon-end"
+               href="#" target="_self" rel="noopener"><span>Learn more</span>
+                <!-- wp:printmasters/icon {"icon":"chevron_right","iconSize":20} -->
+                <div class="wp-block-printmasters-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 -960 960 960">
+                        <path fill="currentColor"
+                              d="m517.85-480-184-184L376-706.15 602.15-480 376-253.85 333.85-296l184-184Z"></path>
+                    </svg>
+                </div>
+                <!-- /wp:printmasters/icon --></a>
+            <!-- /wp:printmasters/button --></div>
+        <!-- /wp:printmasters/buttons --></div>
+    <!-- /wp:printmasters/flex-item -->
+
+    <!-- wp:printmasters/flex-item -->
+    <div class="wp-block-printmasters-flex-item items-stretch tablet--items-stretch desktop--items-stretch content-start tablet--content-start desktop--content-start flex-row tablet--flex-col desktop--flex-col justify-start tablet--justify-start desktop--justify-start flex-no-wrap tablet--flex-no-wrap desktop--flex-no-wrap self-auto tablet--self-auto desktop--self-auto grow-0 tablet--grow-0 desktop--grow-0 shrink tablet--shrink desktop--shrink order-0 tablet--order-0 desktop--order-0"
+         style="--flex-basis:100%;--flex-basis-tablet:100%;--flex-basis-desktop:50%">
+        <!-- wp:cover {"overlayColor":"primary","isUserOverlayColor":true,"minHeight":44,"minHeightUnit":"rem","layout":{"type":"constrained"}} -->
+        <div class="wp-block-cover" style="min-height:44rem"><span aria-hidden="true"
+                                                                   class="wp-block-cover__background has-primary-background-color has-background-dim-100 has-background-dim"></span>
+            <div class="wp-block-cover__inner-container">
+                <!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+                <p class="has-text-align-center has-large-font-size">Image</p>
+                <!-- /wp:paragraph --></div>
+        </div>
+        <!-- /wp:cover --></div>
+    <!-- /wp:printmasters/flex-item --></div>
+<!-- /wp:printmasters/flex-container -->
---
+++ patterns/hero-02.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Title: Hero #02
+ * Slug: printmasters/hero-02
+ * Categories: pm
+ * Viewport width: 1200
+ */
+?>
+
+<!-- wp:printmasters/flex-container {"globalMarginBottom":false,"metadata":{"categories":["pm"],"patternName":"core/block/465"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|16"}}}} -->
+<div class="wp-block-printmasters-flex-container items-stretch tablet--items-stretch desktop--items-stretch content-start tablet--content-start desktop--content-start flex-row tablet--flex-col desktop--flex-col justify-start tablet--justify-start desktop--justify-start flex-no-wrap tablet--flex-no-wrap desktop--flex-no-wrap"
+     style="margin-top:var(--wp--preset--spacing--16)">
+    <!-- wp:printmasters/flex-item {"basis":{"basisMobile":100,"basisTablet":100,"basisDesktop":"100"}} -->
+    <div class="wp-block-printmasters-flex-item items-stretch tablet--items-stretch desktop--items-stretch content-start tablet--content-start desktop--content-start flex-row tablet--flex-col desktop--flex-col justify-start tablet--justify-start desktop--justify-start flex-no-wrap tablet--flex-no-wrap desktop--flex-no-wrap self-auto tablet--self-auto desktop--self-auto grow-0 tablet--grow-0 desktop--grow-0 shrink tablet--shrink desktop--shrink order-0 tablet--order-0 desktop--order-0"
+         style="--flex-basis:100%;--flex-basis-tablet:100%;--flex-basis-desktop:100%">
+        <!-- wp:cover {"overlayColor":"primary","isUserOverlayColor":true,"minHeight":30,"minHeightUnit":"rem","layout":{"type":"constrained"}} -->
+        <div class="wp-block-cover" style="min-height:30rem"><span aria-hidden="true"
+                                                                   class="wp-block-cover__background has-primary-background-color has-background-dim-100 has-background-dim"></span>
+            <div class="wp-block-cover__inner-container">
+                <!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+                <p class="has-text-align-center has-large-font-size">Image</p>
+                <!-- /wp:paragraph --></div>
+        </div>
+        <!-- /wp:cover --></div>
+    <!-- /wp:printmasters/flex-item --></div>
+<!-- /wp:printmasters/flex-container -->
+
+<!-- wp:printmasters/flex-container -->
+<div class="wp-block-printmasters-flex-container items-stretch tablet--items-stretch desktop--items-stretch content-start tablet--content-start desktop--content-start flex-row tablet--flex-col desktop--flex-col justify-start tablet--justify-start desktop--justify-start flex-no-wrap tablet--flex-no-wrap desktop--flex-no-wrap has-global-margin-bottom">
+    <!-- wp:printmasters/flex-item {"basis":{"basisMobile":100,"basisTablet":100,"basisDesktop":"100"}} -->
+    <div class="wp-block-printmasters-flex-item items-stretch tablet--items-stretch desktop--items-stretch content-start tablet--content-start desktop--content-start flex-row tablet--flex-col desktop--flex-col justify-start tablet--justify-start desktop--justify-start flex-no-wrap tablet--flex-no-wrap desktop--flex-no-wrap self-auto tablet--self-auto desktop--self-auto grow-0 tablet--grow-0 desktop--grow-0 shrink tablet--shrink desktop--shrink order-0 tablet--order-0 desktop--order-0"
+         style="--flex-basis:100%;--flex-basis-tablet:100%;--flex-basis-desktop:100%"><!-- wp:printmasters/buttons -->
+        <div class="wp-block-printmasters-buttons justify-start tablet--justify-start desktop--justify-start">
+            <!-- wp:printmasters/button {"buttonText":"Our story","showIcon":false,"backgroundColor":"neutral-500"} -->
+            <a class="wp-block-printmasters-button has-neutral-500-background-color has-background is-filled has-icon-end"
+               href="#" target="_self" rel="noopener"><span>Our story</span></a>
+            <!-- /wp:printmasters/button -->
+
+            <!-- wp:printmasters/button {"buttonText":"Mission and vision","showIcon":false,"style":{"elements":{"link":{"color":{"text":"var:preset|color|neutral-400"}}}},"backgroundColor":"neutral-200","textColor":"neutral-400"} -->
+            <a class="wp-block-printmasters-button has-neutral-400-color has-neutral-200-background-color has-text-color has-background has-link-color is-filled has-icon-end"
+               href="#" target="_self" rel="noopener"><span>Mission and vision</span></a>
+            <!-- /wp:printmasters/button -->
+
+            <!-- wp:printmasters/button {"buttonText":"Team","showIcon":false,"style":{"elements":{"link":{"color":{"text":"var:preset|color|neutral-400"}}}},"backgroundColor":"neutral-200","textColor":"neutral-400"} -->
+            <a class="wp-block-printmasters-button has-neutral-400-color has-neutral-200-background-color has-text-color has-background has-link-color is-filled has-icon-end"
+               href="#" target="_self" rel="noopener"><span>Team</span></a>
+            <!-- /wp:printmasters/button -->
+
+            <!-- wp:printmasters/button {"buttonText":"Departments","showIcon":false,"style":{"elements":{"link":{"color":{"text":"var:preset|color|neutral-400"}}}},"backgroundColor":"neutral-200","textColor":"neutral-400"} -->
+            <a class="wp-block-printmasters-button has-neutral-400-color has-neutral-200-background-color has-text-color has-background has-link-color is-filled has-icon-end"
+               href="#" target="_self" rel="noopener"><span>Departments</span></a>
+            <!-- /wp:printmasters/button -->
+
+            <!-- wp:printmasters/button {"buttonText":"CEO’s message","showIcon":false,"style":{"elements":{"link":{"color":{"text":"var:preset|color|neutral-400"}}}},"backgroundColor":"neutral-200","textColor":"neutral-400"} -->
+            <a class="wp-block-printmasters-button has-neutral-400-color has-neutral-200-background-color has-text-color has-background has-link-color is-filled has-icon-end"
+               href="#" target="_self" rel="noopener"><span>CEO’s message</span></a>
+            <!-- /wp:printmasters/button --></div>
+        <!-- /wp:printmasters/buttons --></div>
+    <!-- /wp:printmasters/flex-item --></div>
+<!-- /wp:printmasters/flex-container -->
