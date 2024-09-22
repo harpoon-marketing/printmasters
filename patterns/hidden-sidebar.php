787f54a21e35c6148e170f62d56dc69e6231e357
Marius P. <marius.paduraru@gmail.com>
2024-09-22 19:29:37 +0100

    Add sidebar and custom template
    Introduce a hidden sidebar pattern, related styles, and a "Page with Sidebar" custom template. Adjust layout and transition properties in `theme.json`, and enhance the main template structure to integrate the new sidebar component. Refactor video initialization in JavaScript to improve code organization.

---
+++ patterns/hidden-sidebar.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Title: Sidebar
+ * Slug: printmasters/hidden-sidebar
+ * Inserter: no
+ */
+?>
+
+<!-- wp:group {"className":"printmasters-sidebar","layout":{"type":"constrained"}} -->
+<div class="wp-block-group printmasters-sidebar">
+    <!-- wp:buttons {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+    <div class="wp-block-buttons">
+        <!-- wp:button {"backgroundColor":"neutral-100","textColor":"neutral-500","style":{"elements":{"link":{"color":{"text":"var:preset|color|neutral-500"}}},"border":{"radius":"0px"}}} -->
+        <div class="wp-block-button"><a
+                    class="wp-block-button__link has-neutral-500-color has-neutral-100-background-color has-text-color has-background has-link-color wp-element-button"
+                    style="border-radius:0px">Link One</a></div>
+        <!-- /wp:button -->
+
+        <!-- wp:button {"backgroundColor":"neutral-100","textColor":"neutral-500","style":{"elements":{"link":{"color":{"text":"var:preset|color|neutral-500"}}},"border":{"radius":"0px"}}} -->
+        <div class="wp-block-button"><a
+                    class="wp-block-button__link has-neutral-500-color has-neutral-100-background-color has-text-color has-background has-link-color wp-element-button"
+                    style="border-radius:0px">Link Two</a></div>
+        <!-- /wp:button -->
+
+        <!-- wp:button {"backgroundColor":"neutral-100","textColor":"neutral-500","style":{"elements":{"link":{"color":{"text":"var:preset|color|neutral-500"}}},"border":{"radius":"0px"}}} -->
+        <div class="wp-block-button"><a
+                    class="wp-block-button__link has-neutral-500-color has-neutral-100-background-color has-text-color has-background has-link-color wp-element-button"
+                    style="border-radius:0px">Link Tree</a></div>
+        <!-- /wp:button -->
+
+        <!-- wp:button {"backgroundColor":"neutral-100","textColor":"neutral-500","style":{"elements":{"link":{"color":{"text":"var:preset|color|neutral-500"}}},"border":{"radius":"0px"}}} -->
+        <div class="wp-block-button"><a
+                    class="wp-block-button__link has-neutral-500-color has-neutral-100-background-color has-text-color has-background has-link-color wp-element-button"
+                    style="border-radius:0px">Link Four</a></div>
+        <!-- /wp:button -->
+
+        <!-- wp:button {"backgroundColor":"neutral-100","textColor":"neutral-500","style":{"elements":{"link":{"color":{"text":"var:preset|color|neutral-500"}}},"border":{"radius":"0px"}}} -->
+        <div class="wp-block-button"><a
+                    class="wp-block-button__link has-neutral-500-color has-neutral-100-background-color has-text-color has-background has-link-color wp-element-button"
+                    style="border-radius:0px">Link Five</a></div>
+        <!-- /wp:button -->
+
+        <!-- wp:button {"backgroundColor":"neutral-100","textColor":"neutral-500","style":{"elements":{"link":{"color":{"text":"var:preset|color|neutral-500"}}},"border":{"radius":"0px"}}} -->
+        <div class="wp-block-button"><a
+                    class="wp-block-button__link has-neutral-500-color has-neutral-100-background-color has-text-color has-background has-link-color wp-element-button"
+                    style="border-radius:0px">Link Six</a></div>
+        <!-- /wp:button --></div>
+    <!-- /wp:buttons --></div>
+<!-- /wp:group -->
--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,65 +1,72 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const allVideos = document.querySelectorAll(".wp-block-video");
+  const initVideo = () => {
+    const allVideos = document.querySelectorAll(".wp-block-video");
 
-  const playIcon =
+    const playIcon =
       "<svg xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 -960 960 960' width='24px' fill='#e8eaed'><path d='M340-236.16v-487.68L723.07-480 340-236.16Z'/></svg>";
 
-  const pauseIcon =
+    const pauseIcon =
       "<svg xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 -960 960 960' width='24px' fill='#e8eaed'><path d='M560-220v-520h150v520H560Zm-310 0v-520h150v520H250Z'/></svg>";
 
-  /**
-   * Renders video controls in the specified wrapper element.
-   *
-   * @param {HTMLElement} wrapper - The wrapper element to append the controls to.
-   * @param {string} action - The action to be performed by the controls (e.g., "play", "pause").
-   * @returns {HTMLElement} - The created controls' element.
-   */
-  const renderControls = (wrapper, action) => {
-    const controls = document.createElement("div");
-    controls.classList.add("printmasters-video-controls", "show");
+    /**
+     * Renders video controls in the specified wrapper element.
+     *
+     * @param {HTMLElement} wrapper - The wrapper element to append the controls to.
+     * @param {string} action - The action to be performed by the controls (e.g., "play", "pause").
+     * @returns {HTMLElement} - The created controls' element.
+     */
+    const renderControls = (wrapper, action) => {
+      const controls = document.createElement("div");
+      controls.classList.add("printmasters-video-controls", "show");
 
-    controls.innerHTML = action === "play" ? playIcon : pauseIcon;
-
-    wrapper.appendChild(controls);
-
-    return controls;
-  };
-
-  allVideos.forEach((wrapper) => {
-    const video = wrapper.querySelector("video");
-    video.controls = false;
-    const controls = renderControls(wrapper, "play");
-
-    const updateControls = (action) => {
       controls.innerHTML = action === "play" ? playIcon : pauseIcon;
+
+      wrapper.appendChild(controls);
+
+      return controls;
     };
 
-    controls.addEventListener("click", () => {
-      if (video.paused) {
-        video.play();
+    allVideos.forEach((wrapper) => {
+      const video = wrapper.querySelector("video");
+      video.controls = false;
+      const controls = renderControls(wrapper, "play");
+
+      const updateControls = (action) => {
+        controls.innerHTML = action === "play" ? playIcon : pauseIcon;
+      };
+
+      controls.addEventListener("click", () => {
+        if (video.paused) {
+          video.play();
+          updateControls("pause");
+        } else {
+          video.pause();
+          updateControls("play");
+        }
+      });
+
+      video.addEventListener("play", () => {
         updateControls("pause");
-      } else {
-        video.pause();
+      });
+
+      video.addEventListener("pause", () => {
         updateControls("play");
-      }
-    });
+      });
 
-    video.addEventListener("play", () => {
-      updateControls("pause");
-    });
+      wrapper.addEventListener("mouseenter", () => {
+        controls.classList.add("show");
+      });
 
-    video.addEventListener("pause", () => {
-      updateControls("play");
-    });
-
-    wrapper.addEventListener("mouseenter", () => {
-      controls.classList.add("show");
-    });
-
-    wrapper.addEventListener("mouseleave", () => {
-      video.paused
+      wrapper.addEventListener("mouseleave", () => {
+        video.paused
           ? controls.classList.add("show")
           : controls.classList.remove("show");
+      });
     });
-  });
+  };
+
+  const initSidebar = () => {};
+
+  initVideo();
+  initSidebar();
 });
